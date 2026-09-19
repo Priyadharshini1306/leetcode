a1c14423b43def4bb6dae014b7ec53374fa12d95
PRIYADHARSHINI R <priyadharshinir.ad24@bitsathy.ac.in>
2026-09-19 10:13:42 +0530

    Time: 6 ms (43.73%) | Memory: 46.5 MB (21.29%) - LeetSync

---
+++ 137-single-number-ii/single-number-ii.java
@@ -0,0 +1,14 @@
+class Solution {
+    public int singleNumber(int[] nums) {
+        HashMap<Integer,Integer> m = new HashMap<>();
+        for(int i:nums) {
+            m.put(i,m.getOrDefault(i,0)+1);
+        }
+        for(Map.Entry<Integer, Integer> entry : m.entrySet()) {
+            if(entry.getValue()==1) {
+                return entry.getKey();
+            }
+        }
+        return -1;
+    }
+}
