783e993c85ef85a6a23658e1d7478d3dc38a704f
PRIYADHARSHINI R <priyadharshinir.ad24@bitsathy.ac.in>
2026-09-22 15:57:48 +0530

    Time: 3 ms (14.83%) | Memory: 46.2 MB (84.69%) - LeetSync

---
+++ 4354-unique-middle-element/unique-middle-element.java
@@ -0,0 +1,13 @@
+class Solution {
+    public boolean isMiddleElementUnique(int[] nums) {
+        HashMap<Integer,Integer> map = new HashMap<>();
+        for(int i = 0;i<nums.length;i++) {
+            map.put(nums[i],map.getOrDefault(nums[i],0)+1);
+        }
+        int mid = nums.length/2;
+        if(map.get(nums[mid])==1) {
+            return true;
+        }
+        return false;
+    }
+}
